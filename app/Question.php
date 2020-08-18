726dacd37d5a8f9fbbac3c80189b29357a52f9ce
tsaroj <saroj073bct@ioepc.edu.np>
2020-08-18 21:59:09 +0545

    add author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -9,13 +9,21 @@ class Question extends Model
 {
 
     protected $fillable = ['title','body'];
-    public function users(){
+    public function user(){
         return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($value){
         $this->attributes['title']= $value;
         $this->attributes['slug']= str::slug($value);    
-     }
+    }
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
 
 }

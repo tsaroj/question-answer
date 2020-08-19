3fb4d6791c54033af68ebc82c9fb55a2a8775728
tsaroj <saroj073bct@ioepc.edu.np>
2020-08-19 23:09:08 +0545

    authorizing th question using gates

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -62,7 +62,10 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {   
-        return view('questions.edit',compact('question'));
+        if (\Gate::denies('update-question', $question)) {
+            abort(403, "Access denied");
+        }
+        return view("questions.edit", compact('question'));
     }
 
     /**
@@ -74,6 +77,9 @@ class QuestionController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if (\Gate::denies('update-question', $question)) {
+            abort(403, "Access denied");
+        }
         $question->update($request->only('title','body'));
         return redirect()->route('questions.index')->with('success','Your Question has been updated.');
     }
@@ -86,6 +92,9 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
+        if (\Gate::denies('delete-question', $question)) {
+            abort(403, "Access denied");
+        }
         $question->delete();
         return redirect()->route('questions.index')->with('success','Your Question has been Deleted.');
     }

1caf106562ffe717f04c106176934b36219b13a2
tsaroj <saroj073bct@ioepc.edu.np>
2020-08-25 20:46:34 +0545

    implementing the seleting best answer with delete in answer

--- app/Answer.php
+++ app/Answer.php
@@ -39,4 +39,9 @@ class Answer extends Model
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '' ;
+    }
 }

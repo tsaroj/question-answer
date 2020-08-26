eccf119fef1df4f259b578ad8bfeca36c1671a05
tsaroj <saroj073bct@ioepc.edu.np>
2020-08-27 00:17:35 +0545

    Implementing the many to many relation and  make favoriteing the question

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       $this->acll([
+       $this->call([
            UsersQuestionsAnswersTableSeeder::class,
            FavoritesTableSeeder::class,
        ]);

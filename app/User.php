67fbf7dcc45dddecd07063426ebc3b603a7ecc63
tsaroj <saroj073bct@ioepc.edu.np>
2020-08-27 16:41:49 +0545

    implementing votes system in question ans answers with morphToMany relation

--- app/User.php
+++ app/User.php
@@ -64,6 +64,65 @@ class User extends Authenticatable
         return $this->belongsToMany(Question::class,'favorites')->withTimestamps();//,'user_id','question_id');
     }
 
+    public function voteQuestions()
+    {
+        return $this->morphedByMany(Question::class, 'votable');
+    }
+
+
+    public function voteAnswers()
+    {
+        return $this->morphedByMany(Answer::class,'votable');
+    }
+
+    public function voteQuestion(Question $question,$vote)
+    {
+        $voteQuestions = $this->voteQuestions();
+        if ($voteQuestions->where('votable_id',$question->id)->exists()) {
+            if ($voteQuestions->where('vote',$vote)->exists()) {
+                $voteQuestions->detach($question,['vote'=>$vote]);
+            }
+            else{
+                $voteQuestions->updateExistingPivot($question,['vote'=>$vote]);
+
+            }
+        }
+        else{
+            $voteQuestions->attach($question,['vote'=>$vote]);
+        }
+
+        $question->load('votes');
+        $downVotes = (int) $question->downVotes()->sum('vote');
+        $upVotes = (int) $question->upVotes()->sum('vote');
+        $question->votes_count = $upVotes + $downVotes;
+        $question->save();
+
+    }
+
+    public function voteAnswer(Answer $answer, $vote)
+    {
+         $voteAnswers = $this->voteAnswers();
+        if ($voteAnswers->where('votable_id',$answer->id)->exists()) {
+            if ($voteAnswers->where('vote',$vote)->exists()) {
+                $voteAnswers->detach($answer,['vote'=>$vote]);
+            }
+            else{
+                $voteAnswers->updateExistingPivot($answer,['vote'=>$vote]);
+
+            }
+        }
+        else{
+            $voteAnswers->attach($answer,['vote'=>$vote]);
+        }
+
+        $answer->load('votes');
+        $downVotes = (int) $answer->downVotes()->sum('vote');
+        $upVotes = (int) $answer->upVotes()->sum('vote');
+        $answer->votes_count = $upVotes + $downVotes;
+        $answer->save();
+
+    }
+
     
 
 
